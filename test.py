ab7e9e5ff0b28f03ec8181178f9bb5d40e3b4f6b
malaartyom <malarenkoart@bk.ru>
2023-02-19 18:59:42 +0700

    Add sum

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ a = [1, 2, 3, 4, 5]
 for i in range(5):
     print(a[i])
 a.reverse()
+print()
 for i in range(5):
     print(a[i])
+print()
+for i in range(5):
+    for j in range(5):
+        print(a[i] + a[j])
